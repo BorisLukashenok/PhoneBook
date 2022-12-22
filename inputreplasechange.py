807d15e195cc2fd682c3283ccd29ad299443558d
BorisLukashenok <jnardis@yandex.ru>
2022-12-22 14:52:36 +0300

    Telefon work

---
+++ inputreplasechange.py
@@ -0,0 +1,92 @@
+def input_contakt():
+    return input('Введитe фамилию, имя, телефон, описание(15 символов) через пробел.\n').title().split()
+
+
+def find_string(book):
+    result = []
+    ans = input(
+        "Введите что либо, указывающее на конкретный контакт. Номер телефона, фамилию или имя:\n")
+    for line in book:
+        if ans.lower() in line.lower():
+            result.append(line)
+    return result
+
+
+def print_contact(line):
+    print("-" * len(line))
+    print(line)
+    print("-" * len(line))
+
+
+def search(book):
+    temp = book['str'] + book['lst']
+    ser = find_string(temp)
+    for i in set(ser):
+        if i != -1:
+            print_contact(i)
+
+def find(book):
+    ans = input(
+        "Введите что либо, указывающее на конкретный контакт. Номер телефона, фамилию или имя:\n")
+    for line in book:
+        if ans.lower() in line.lower():
+            return book.index(line)
+    print()
+    print('===>> Такой записи еще нет, если хотите добавить новую, то нажмите 2 или 3! <<===')
+    print()
+    return -1
+
+def delete_contact(book):
+    index = find(book)
+    if index != -1:
+        print_contact(book[index])
+        if input('Этот контакт будет удален!!! 1 - Да, 2 - Нет\n') == '1':
+            print()
+            print("Контакт успешно удален!")
+            print()
+            return book[index]
+        else:
+            print()
+            print("Контакт не будет удален!")
+            print()
+            return ""
+    else:
+        return ''
+
+def change_contact(book):
+    index = find(book)
+    if index != -1:
+        print_contact(book[index])
+        if input('Этот контакт будет изменен!!! 1 - Да, 2 - Нет\n') == '1':
+            old_result = book[index]
+            result = book[index].split(", ")
+            change = input("Что именно вы хотите изменить? Введите 'Имя', 'Фамилия', 'Номер', 'Комментарий':\n ")
+            name = result[0]
+            secondname = result[1]
+            number = result[2]
+            comment = result[3]
+            change_to = input("На что менять?: ")
+            if change.lower() == "имя":
+                result.remove(name)
+                result.insert(0, change_to)
+            elif change.lower() == "фамилия":
+                result.remove(secondname)
+                result.insert(1, change_to)
+            elif change.lower() == "номер":
+                result.remove(number)
+                result.insert(2, change_to)
+            elif change.lower() == "комментарий":
+                result.remove(comment)
+                result.insert(3, change_to)
+            else:
+                print("Некорректный ввод!!!")
+            print_contact(result)
+            print()
+            print("Контакт успешно изменен!")
+            return result, old_result
+        else:
+            print()
+            print("Контакт не будет изменен!")
+            return "", ""
+    else:
+        return '', ''
